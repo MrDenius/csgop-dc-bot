f6f869e6d87e9c15e142afe86eba701a03f08689
Denius <denisa5700@yandex.ru>
2020-12-15 21:33:37 +1000

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const StartIndex = () => {
 	const index = child_process.spawn("node", ["./index.js"]);
 
 	const conLog = (text, log) => {
-		if (process.env.PORT && text.trim()) {
+		if (process.env.PORT && text.trim().length != 0) {
 			if (log) log(text);
 			else console.log(text);
 		}
